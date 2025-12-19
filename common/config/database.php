7ba0c9db5063e2a2641c6296c0d6b99eff49d47f
chenketing123 <1041734003@qq.com>
2025-12-19 17:18:03 +0800

    12-19

--- common/config/database.php
+++ common/config/database.php
@@ -4,9 +4,9 @@
  */
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=pcj',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=pcj_t_v_trend_cn',
+    'username' => 'pcj_t_v_trend_cn',
+    'password' => 'hBEw8MY2BYAYPyHW',
     'charset' => 'utf8',
     'tablePrefix' => 'zs_',
     'enableSchemaCache' => true,
